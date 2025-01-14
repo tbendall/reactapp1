4bdc9125e1d90344c918b31af9df930a9f32c18f
Tristan Bendall <tbendall@users.noreply.github.com>
2025-01-14 14:41:39 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <script src="https://unpkg.com/react@18/umd/react.development.js" crossorigin></script>
-    <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js" crossorigin></script>
-    <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
-  </head>
-  <body>
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-    <div id="mydiv"></div>
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
 
-    <script type="text/babel">
-      function Hello() {
-        return <h1>Hello World!</h1>;
-      }
-
-      const container = document.getElementById('mydiv');
-      const root = ReactDOM.createRoot(container);
-      root.render(<Hello />)
-    </script>
-
-  </body>
-</html>
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />);
